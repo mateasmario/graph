87439f90e4f9bda411c3912594b7eda67da9ea64
Mario-MihaiMateaș <mateasmario@ibm.com>
2025-05-08 21:00:09 +0300

    Updated runner

--- generator/src/main/java/ro/upt/Runner.java
+++ generator/src/main/java/ro/upt/Runner.java
@@ -1,7 +1,19 @@
 package ro.upt;
 
+import ro.upt.graph.generator.impl.ConnectedGraphGenerator;
+import ro.upt.graph.generator.impl.WeightedConnectedGraphGenerator;
+import ro.upt.graph.writer.GraphWriter;
+import ro.upt.graph.writer.impl.FileGraphWriter;
+
+import java.io.IOException;
+
 public class Runner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("Usage: graph <noEdges> <outputFile>");
+        }
+
+        new FileGraphWriter(args[1])
+                .writeGraph(Integer.parseInt(args[0]), new WeightedConnectedGraphGenerator().generate(Integer.parseInt(args[0])));
     }
 }
